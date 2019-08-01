2a6ab7596eb485e0532d79c3fa74d807690e0946
Gaurav Kumar Sharma <gs8280601@gmail.com>
2019-08-01 08:07:42 +0000

    New Coomit

--- app.js
+++ app.js
@@ -1,31 +1,8 @@
-var express = require("express")
-var app = express();
+var app = require("express")()
 
-app.get("/",function(req,res){
-    res.send("Hi! welcome to my assignment");
-});
-
-app.get("/speak/:animals/",function(req,res){
-    if(req.params.animals == "pig"){
-        res.send("Oink");
-    }
-    else if(req.params.animals == "cow"){
-        res.send("Moooo");
-    }
-    else if(req.params.animals == "dog"){
-        res.send("Woof Woof!");
-    }
-});
-
-app.get("/repeat/:str/:count",function(req,res){
-    var count = Number(req.params.count);
-    res.send(req.params.str.repeat(count));
-});
-
-app.get("*",function(req,res){
-    res.send("Sorry...Page Not Found. What are doing with your life");
+app.get("/results",function(req,res){
+    res.send("HI there");
 })
-
 app.listen(3000,function(){
     console.log("Server is started");
-});
+})
