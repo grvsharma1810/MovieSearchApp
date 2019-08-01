7eff196b3eb910797552e4b09add376c01bb5d61
Gaurav Kumar Sharma <gs8280601@gmail.com>
2019-08-01 18:38:13 +0000

    Last commit

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-var app = require("express")()
+var app = require("express")();
+var request = require("request");
+
+app.set("view engine","ejs");
+
+app.get("/",function(req,res){
+    res.render("search");
+})
 
 app.get("/results",function(req,res){
-    res.send("HI there");
+    var query = req.query.search;
+    request("http://www.omdbapi.com/?i=tt3896198&apikey=dff0c193&s="+query,function(err,response,body){
+        if(!err && response.statusCode==200){
+            result = JSON.parse(body);
+            res.render("results",{result:result});
+        }
+        else{
+            res.send("SOMETHING WENT WRONG");
+        }
+    });
 })
+
+
+app.get("*",function(req,res){
+    res.send("Page Not Found");
+});
 app.listen(3000,function(){
     console.log("Server is started");
 })
